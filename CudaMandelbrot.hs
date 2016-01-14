3b09a9d7882f7f40b671cda027624ed9540fcde1
Justin Dawson <jdawson@ku.edu>
2016-01-14 16:03:11 -0600

    Kinda working (tiling issue)

--- CudaMandelbrot.hs
+++ CudaMandelbrot.hs
@@ -11,14 +11,14 @@ import qualified Data.ByteString as BS
 import Data.Bits as Bits
 import qualified Data.Vector.Unboxed as V
 
-step =  0.0025
-
 mandelbrot
-    ::  forall a. (Elt a, IsFloating a) => (Exp a,Exp a,Exp a, Exp a)
+    ::  forall a. (Elt a, IsFloating a)
+    => Exp a
+    -> (Exp a,Exp a,Exp a, Exp a)
     -> Int
     -> Acc (Array DIM2 Int32)
-mandelbrot (xmin,ymin,xmax,ymax) depth =
-  generate (constant (Z:.screenY:.screenX))
+mandelbrot stepA (xmin,ymin,xmax,ymax) depth =
+  generate ((index2 screenY screenX))
            (\ix -> let c = initial ix
                        iter = A.snd $ A.while (\zi -> A.snd zi A.<* lIMIT &&* dot (A.fst zi) A.<* 4) (\zi -> lift1 (next c) zi) (lift (c, constant 0))
                     in  iter
@@ -26,11 +26,10 @@ mandelbrot (xmin,ymin,xmax,ymax) depth =
                                        
   where
     -- The view plane
-    stepA   :: Exp a          = constant (A.fromIntegral step)
     sizex   :: Exp a          =  stepA
     sizey   :: Exp a          =  stepA
-    screenX :: Int          =  P.fromIntegral $ ((1+) . abs . A.round) $ (xmax - xmin) / stepA
-    screenY :: Int          =  P.fromIntegral $ ((1+) . abs . A.round) $ (ymin -ymax) / stepA
+    screenX :: Exp Int        =  (abs . A.round) $ (xmax - xmin) / stepA
+    screenY :: Exp Int        =  (abs . A.round) $ (ymin -ymax) / stepA
     -- initial conditions for a given pixel in the window, translated to the
     -- corresponding point in the complex plane
     initial ::  Exp DIM2 -> Exp (Complex a)
@@ -53,12 +52,19 @@ main :: IO()
 main = blankCanvas 3000 {middleware=[]} $ \ context -> do
           putStrLn "Start Request"
           start <- getCurrentTime
-          let res =  mandelbrot (A.constant (-2)::Exp Double,A.constant (-1) :: Exp Double ,A.constant 0.5:: Exp Double,A.constant 1:: Exp Double) 255 
-          let h =  ((1+) . abs . P.round) $ ((-1) -1) / (step)
-          let w = ((1+) . abs . P.round) $ (0.5 -(-2)) / (step)
+          let step = 0.00025
+         -- let res =  mandelbrot (A.constant step)(A.constant (-2)::Exp Double,A.constant (-1) :: Exp Double ,A.constant 0.5:: Exp Double,A.constant 1:: Exp Double) 255 
+          let (xmin,ymin,xmax,ymax) = (-1,-1,0,0.5)
+          let res =  mandelbrot (A.constant step)(A.constant xmin::Exp Double,A.constant ymin :: Exp Double ,A.constant xmax:: Exp Double,A.constant ymax:: Exp Double) 255 
+          let h :: Int =  (abs . P.round) $ (ymax - ymin) / ( step )
+          let w ::Int= (abs . P.round) $ (xmax - xmin) / ( step ) 
           let ans = run res 
           let iters = P.map (\v -> let word = P.fromIntegral v 
                                  in  BS.foldl (\acc x -> (acc `Bits.shiftL` 8) .|. (P.fromIntegral x)) zeroBits $ BS.pack $ [word,0,word])  $ toList ans
+          putStr "W: "
+          print $ w
+          putStr "H: "
+          print $ h
           send context $ putImageData (ImageData w h (V.fromList (iters)), [0,0])
  
           stop <- getCurrentTime
